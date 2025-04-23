fcd9d3050acc12cbde9aa4e7cdbac6f3864f29ec
ssje <2114714800@qq.com>
2025-04-23 18:13:16 +0800

    小修改

--- src/learn_06_10/learn_10/learn_live.rs
+++ src/learn_06_10/learn_10/learn_live.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 pub fn test() {
-    test001();
+    test002();
 }
 
 //函数签名中的生命周期标注
@@ -11,12 +11,14 @@ fn test001() {
     let string1 = String::from("abcd");
     let result;
     {
-        let string2 = "xyz";
-        // let string2 = String::from("xyz");
+        let string2 = "xyz"; //string2是字符串slice 。 xyz是字符串字面量，具有 'static 生命周期，即在整个程序运行期间都有效
+        // let string2 = String::from("xyz"); // `string2` does not live long enough. borrowed value does not live long enough
         result = longest(string1.as_str(), string2);
     }
     println!("The longest string is {}", result);
 }
+
+//结构体定义中的生命周期标注
 fn test002() {
     struct ImportantExcerpt<'a> {
         part: &'a str,
@@ -26,3 +28,5 @@ fn test002() {
     let i = ImportantExcerpt { part: a2 };
     println!("ImportantExcerpt: {}", i.part);
 }
+
+fn test003() {}
