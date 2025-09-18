a6d9cc8190cd5331a2d04fce98f5bd89379575b0
ssje <2114714800@qq.com>
2025-09-18 18:09:57 +0800

    尝试了一点多线程修改，互斥锁

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,27 @@
 #![allow(unused)]
+use std::clone;
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
-pub fn test() {}
+pub fn test() {
+    let v = Arc::new(Mutex::new(vec![]));
+    let mut handles: Vec<JoinHandle<()>> = vec![];
+    for i in 0..5 {
+        let v: Arc<Mutex<Vec<i32>>> = Arc::clone(&v);
+        let handle: JoinHandle<()> = thread::spawn(move || {
+            if let Ok(mut vec) = v.lock() {
+                vec.push(i);
+            } else {
+                eprintln!("Failed to acquire lock");
+            }
+        });
+        handles.push(handle);
+    }
+    for h in handles {
+        h.join().unwrap();
+    }
+    println!("Result: {:?}", *v.lock().unwrap());
+}
 
 struct Color(u8, u8, u8, f32);
 impl Color {
